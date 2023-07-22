a6c078746431b9b26a22af8cae16af344980fa31
andz89m <andzrivero89@gmail.com>
2023-07-21 23:40:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from 'react'
+import ImageCard from './components/imageCard';
+import Header from './components/Header';
 
 function App() {
+  const [images, setImages] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [term, setTerm] = useState("")
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXIBAY_API_KEY}&per_page=${30}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+
+        setImages(data.hits);
+        setLoading(false)
+      })
+      .catch(err => console.log(err))
+  }, [term])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+      <Header searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32 text-gray-500">No Images Found</h1>}
+      <div className="container mx-auto">
+        {isLoading ? <h1 className="text-6xl text-center mx-auto ">Loading</h1> :
+          <>
+            <div className="grid grid-cols-3 gap-2 mt-10 px-10 ">
+
+              {images.map(image => (
+                <ImageCard key={image.id} image={image} />
+              ))}
+            </div></>
+        }
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { BsSearch } from 'react-icons/bs';
+const Header = ({ searchText }) => {
+    const [text, setText] = useState('')
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(`from header -- ${text}`);
+        searchText(text)
+    }
+    return (
+        <div className='bg-orange-500 px-3 py-3  mb-2 flex text-2xl gap-5'
+        ><strong className="text-bold text-white">Gwill Image Gallery </strong>
+            <form onSubmit={onSubmit}>
+                <label className="relative block">
+                    <span className="sr-only">Search</span>
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <BsSearch className='text-stone-400' />
+                    </span>
+                    <input onChange={(e) => setText(e.target.value)} className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search images..." type="text" name="search" />
+                </label>
+            </form>
+        </div>
+    )
+}
+
+export default Header 
