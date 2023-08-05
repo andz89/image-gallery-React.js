348f481f7c4ade58db552f93866c0aaaba57669d
andz89m <andzrivero89@gmail.com>
2023-08-05 00:30:18 -0800

    update v.1.2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -20,9 +19,7 @@ root.render(
     <Router>
       <QueryClientProvider client={queryClient}>
         <App />
-        {/* <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'} /> Add this line */}
       </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
-// swDev();
